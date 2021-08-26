2a2b6b5f5964082f40c7117d15fb448009dbbf8a
mishaelajay <mishaelajay@gmail.com>
2021-08-26 11:34:51 +0530

    Moving constants to a separate file

--- classifier.py
+++ classifier.py
@@ -5,20 +5,14 @@ import cv2
 import urllib.request
 import numpy as np
 import time
+import constants
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Disable Tensorflow logging
 
-model_url = 'https://tfhub.dev/google/aiy/vision/classifier/birds_V1/1'
-labels_url = 'https://www.gstatic.com/aihub/tfhub/labelmaps/aiy_birds_V1_labelmap.csv'
-
-image_urls = [
-    'https://upload.wikimedia.org/wikipedia/commons/c/c8/Phalacrocorax_varius_-Waikawa%2C_Marlborough%2C_New_Zealand-8.jpg',
-    'https://quiz.natureid.no/bird/db_media/eBook/679edc606d9a363f775dabf0497d31de8c3d7060.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/8/81/Eumomota_superciliosa.jpg',
-    'https://i.pinimg.com/originals/f3/fb/92/f3fb92afce5ddff09a7370d90d021225.jpg',
-    'https://cdn.britannica.com/77/189277-004-0A3BC3D4.jpg'
-]
+image_urls = constants.IMAGE_URLS
+model_url = constants.MODEL_URL
+labels_url = constants.LABELS_URL
 
 
 class BirdClassifier:
