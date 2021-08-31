b282ec96bfb5e2ea59a734858a7f119c434859f2
mishaelajay <mishaelajay@gmail.com>
2021-08-31 15:32:52 +0530

    Splitting into separate classes

--- app/classifier.py
+++ app/classifier.py
@@ -2,8 +2,6 @@
 bird classifier
 """
 import os
-from re import L
-import urllib.request
 import time
 import numpy as np
 import constants
@@ -18,12 +16,12 @@ IMAGE_URLS = constants.IMAGE_URLS
 MODEL_URL = constants.MODEL_URL
 LABELS_URL = constants.LABELS_URL
 
-"""
-bird classifier
-"""
 class BirdClassifier:
+    """ For classifying birds """
+
     @classmethod
     def order_birds_by_result_score(cls, model_raw_output, bird_labels):
+        """ Ordering results from model """
         for index, value in np.ndenumerate(model_raw_output):
             bird_index = index[1]
             bird_labels[bird_index]['score'] = value
@@ -32,33 +30,38 @@ class BirdClassifier:
 
     @classmethod
     def get_top_n_result(cls, top_index, birds_names_with_results_ordered):
+        """ Get top n results from ordered results """
         bird_name = birds_names_with_results_ordered[top_index*(-1)][1]['name']
         bird_score = birds_names_with_results_ordered[top_index*(-1)][1]['score']
         return bird_name, bird_score
 
     @classmethod
-    def load_model_and_clean_labels(self, model_url, labels_url):
-        bird_model = Model(MODEL_URL).loaded_model
-        bird_labels = Labels(LABELS_URL).load_and_cleanup()
+    def load_model_and_clean_labels(cls, model_url, labels_url):
+        """ Load and cleanup labels and load model """
+        bird_model = Model(model_url)
+        bird_model.load()
+        bird_labels = Labels(labels_url).load_and_cleanup()
         return bird_model, bird_labels
 
     @classmethod
-    def load_and_prep_image(self, url):
+    def load_and_prep_image(cls, url):
+        """ Fetch image and resize for model """
         image = Image(url)
         image_array = image.load()
         pre_processed_image = image.pre_process(image_array)
         return image.generate_image_tensor(pre_processed_image)
 
     def get_results_for_file(self, url):
+        """ Get all results for a given image file """
         bird_model, bird_labels = self.load_model_and_clean_labels(MODEL_URL, LABELS_URL)
         image_tensor = self.load_and_prep_image(url)
-        model_raw_output = bird_model.call(image_tensor).numpy()
+        model_raw_output = bird_model.predict(image_tensor)
         birds_names_with_results_ordered = \
             self.order_birds_by_result_score(model_raw_output, bird_labels)
         return birds_names_with_results_ordered
 
     def print_results(self, index, birds_names_with_results_ordered):
-        # Print results to kubernetes log
+        """ Print results to kubernetes log """
         print('Run: %s' % int(1 + index))
         bird_name, bird_score = self.get_top_n_result(1, birds_names_with_results_ordered)
         print('Top match: "%s" with score: %s' % (bird_name, bird_score))
@@ -70,6 +73,7 @@ class BirdClassifier:
 
 
     def main(self):
+        """ Main function """
         for index, image_url in enumerate(IMAGE_URLS):
             birds_names_with_results_ordered = self.get_results_for_file(image_url)
             self.print_results(index, birds_names_with_results_ordered)
--- app/model.py
+++ app/model.py
@@ -1,7 +1,16 @@
-
+""" Model class to handle model related operations """
 import tensorflow_hub as hub
 
 class Model:
-    """Model class to handle model related operations"""
+    """ Model class to handle model related operations """
     def __init__(self, model_url):
-       self.loaded_model = hub.KerasLayer(model_url)
+        self.model_url = model_url
+
+    def load(self):
+        """ Load model using model url """
+        self.loaded_model = hub.KerasLayer(self.model_url)
+        return self.loaded_model
+
+    def predict(self, image_tensor):
+        """ Predict image tensor """
+        return self.loaded_model.call(image_tensor).numpy()
