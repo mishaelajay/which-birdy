edb54cbeb95e67b6dc5745f1dbbabe9af98dfad7
Ajay Mishael <mishaelajay@gmail.com>
2021-08-31 14:21:48 +0530

    Containerize fast api (#4)
    * Moving to app folder
    
    * Using uvicorn-gunicorn-fastapi-docker
    
    * Using uvicorn-gunicorn-fastapi-docker

---
+++ app/main.py
@@ -0,0 +1,24 @@
+# Library imports
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from typing import List
+from classifier import BirdClassifier
+
+# Create the app object
+app = FastAPI()
+
+# Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, Bird enthusiast'}
+
+# Takes image url as input and returns top 3 results as output
+@app.get('/which-bird/image')
+async def classify_bird(image_urls: List[str]):
+    BC = BirdClassifier()
+    ordered_results = BC.get_results_for_file(image_urls[0])
+    return {}
+
+# Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
