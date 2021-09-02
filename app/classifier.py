f43c1b47476f1210d8e3a67a5dde971c08bc9e17
mishaelajay <mishaelajay@gmail.com>
2021-09-02 14:53:33 +0530

    Loading model and labels only once

--- app/classifier.py
+++ app/classifier.py
@@ -2,6 +2,7 @@
 bird classifier
 """
 import os
+import pdb
 import time
 import numpy as np
 import constants
@@ -12,12 +13,33 @@ from labels import Labels
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Disable Tensorflow logging
 
-IMAGE_URLS = constants.IMAGE_URLS
-MODEL_URL = constants.MODEL_URL
-LABELS_URL = constants.LABELS_URL
+DEFAULT_IMAGE_URLS = constants.IMAGE_URLS
+DEFAULT_MODEL_URL = constants.MODEL_URL
+DEFAULT_LABELS_URL = constants.LABELS_URL
 
 class BirdClassifier:
     """ For classifying birds """
+    def __init__(self, model, cleaned_labels):
+        self.model = model
+        self.cleaned_labels = cleaned_labels
+
+    @staticmethod
+    def fetch_or_load_model(self, model_url=DEFAULT_MODEL_URL):
+        """ Load model if not already loaded in init """
+        if self.model:
+            return self.model
+        else:
+            bird_model = Model(model_url)
+            return bird_model.load()
+
+    @staticmethod
+    def fetch_or_load_clean_labels(self, labels_url=DEFAULT_LABELS_URL):
+        """ Load and cleanup labels if not already loaded and cleaned in init """
+        if self.labels:
+            return self.labels
+            
+        bird_labels = Labels(labels_url).load_and_cleanup()
+        return bird_labels
 
     @classmethod
     def order_birds_by_result_score(cls, model_raw_output, bird_labels):
@@ -36,14 +58,6 @@ class BirdClassifier:
         return bird_name, bird_score
 
     @classmethod
-    def load_model_and_clean_labels(cls, model_url, labels_url):
-        """ Load and cleanup labels and load model """
-        bird_model = Model(model_url)
-        bird_model.load()
-        bird_labels = Labels(labels_url).load_and_cleanup()
-        return bird_model, bird_labels
-
-    @classmethod
     def load_and_prep_image(cls, url):
         """ Fetch image and resize for model """
         image = Image(url)
@@ -51,14 +65,24 @@ class BirdClassifier:
         pre_processed_image = image.pre_process(image_array)
         return image.generate_image_tensor(pre_processed_image)
 
-    def get_results_for_file(self, url):
+    @staticmethod
+    def get_results_for_image(self, url):
         """ Get all results for a given image file """
-        bird_model, bird_labels = self.load_model_and_clean_labels(MODEL_URL, LABELS_URL)
+        bird_model = self.fetch_or_load_model()
+        bird_labels = self.fetch_or_load_clean_labels()
         image_tensor = self.load_and_prep_image(url)
         model_raw_output = bird_model.predict(image_tensor)
         birds_names_with_results_ordered = \
             self.order_birds_by_result_score(model_raw_output, bird_labels)
         return birds_names_with_results_ordered
+
+    @staticmethod
+    def get_results_for_images(self, urls):
+        result_list = []
+        for image_url in enumerate(urls):
+            birds_names_with_results_ordered = self.get_results_for_image(image_url)
+            result_list.append(birds_names_with_results_ordered)
+        return result_list
 
     def print_results(self, index, birds_names_with_results_ordered):
         """ Print results to kubernetes log """
@@ -75,7 +99,7 @@ class BirdClassifier:
     def main(self):
         """ Main function """
         for index, image_url in enumerate(IMAGE_URLS):
-            birds_names_with_results_ordered = self.get_results_for_file(image_url)
+            birds_names_with_results_ordered = self.get_results_for_image(image_url)
             self.print_results(index, birds_names_with_results_ordered)
 
 if __name__ == "__main__":
--- app/main.py
+++ app/main.py
@@ -1,18 +1,46 @@
-from fastapi import FastAPI, File, UploadFile
-from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
 from classifier import BirdClassifier
+import uvicorn
+import pdb
+from model import Model
+from labels import Labels
+import constants
 
 # Create the app object
 app = FastAPI()
 
+# Load the model
+model = Model(constants.MODEL_URL)
+model.load()
+
+# Load and clean labels
+labels = Labels(constants.LABELS_URL)
+cleaned_labels = labels.load_and_cleanup()
+
+# Load the main classifier withmodel and labels
+bird_classifier = BirdClassifier(model, cleaned_labels)
+
+# input class for image urls
+class BirdInput(BaseModel):
+    image_urls: Optional[List[str]] = None
+    number_of_predictions: Optional[int] = 3
+
 # Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
-    return {'message': 'Hello, Bird enthusiast'}
+    return {'message': 'Hello, Bird enthusiast. Checkout our docs page by appending /docs to the current url'}
 
 # Takes image url as input and returns top 3 results as output
-@app.get('/which-bird/image')
-async def classify_bird(image_urls: List[str]):
-    BC = BirdClassifier()
-    ordered_results = BC.get_results_for_file(image_urls[0])
+@app.post('/which-bird')
+async def classify_bird(images: List[BirdInput]):
+    pdb.set_trace()
+    ordered_results = bird_classifier.get_results_for_images(images.image_urls)
     return {}
+
+def form_result_dict(ordered_results):
+    return {}
+
+if __name__ == "__main__":
+    uvicorn.run(app, debug=True)
